47a94652d18b4cc2467c4246e805a3300b236037
hff <1938304905@qq.com>
2024-09-04 13:47:11 +0800

    fix java bug

--- java-spring-boot/src/main/java/io/github/holmofy/spring/App.java
+++ java-spring-boot/src/main/java/io/github/holmofy/spring/App.java
@@ -1,7 +1,12 @@
 package io.github.holmofy.spring;
 
+import jakarta.annotation.Resource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.SingleColumnRowMapper;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @SpringBootApplication
